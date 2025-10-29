51251c3b59dab074002b760d4b769fe10f2fb7f2
mmauriciobastos <mauriciofib@gmail.com>
2025-10-29 10:59:17 -0700

    fix: remove bottom background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,7 +115,7 @@ export default function Home() {
           )}
         </div>
 
-        <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+        <div className="inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
           <div className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]" />
         </div>
       </div>
